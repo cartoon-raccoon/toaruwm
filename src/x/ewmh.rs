52fecfd20474c16a4de40da61fe382e216c85673
cartoon-raccoon <shaughn.chan@gmail.com>
2021-05-17 00:09:37 +0800

    Updated ewmh and icccm traits

--- src/x/ewmh.rs
+++ src/x/ewmh.rs
@@ -1,5 +1,9 @@
-use super::XConn;
+use super::{XWindowID, XConn};
+use crate::core::types::{Atom, NetWindowStates};
 
 pub trait Ewmh: XConn {
-
+    fn get_window_type(&self, window: XWindowID) -> Option<Vec<Atom>>;
+    fn get_window_states(&self, window: XWindowID) -> NetWindowStates;
+    fn set_supported(&self, screen_idx: i32, atoms: &[Atom]);
+    fn set_wm_state(&self, window: XWindowID, atoms: &[Atom]);
 }
--- src/x/icccm.rs
+++ src/x/icccm.rs
@@ -1,5 +1,25 @@
-use super::XConn;
+use super::{XWindowID, XConn};
+use crate::types::{
+    XWinProperties, WindowState, Atom,
+    WmHints, SizeHints,
+};
 
+/// Exposes ICCCM functionality for an object holding an X connection.
+/// For more information on what these methods return, consult the
+/// [ICCCM](https://www.x.org/releases/X11R7.6/doc/xorg-docs/specs/ICCCM/icccm.html)
+/// reference.
+/// 
+/// Warning: DO NOT READ THROUGH EVERYTHING. It is incredibly boring and you _will_
+/// fall asleep. Consult only the parts you need, as needed.
 pub trait Icccm: XConn {
-    
+    fn get_client_properties(&self, window: XWindowID) -> XWinProperties;
+    fn get_wm_name(&self, window: XWindowID) -> String;
+    fn get_wm_icon_name(&self, window: XWindowID) -> String;
+    fn get_wm_size_hints(&self, window: XWindowID) -> Option<SizeHints>;
+    fn get_wm_hints(&self, window: XWindowID) -> Option<WmHints>;    
+    fn get_wm_class(&self, window: XWindowID) -> Option<(String, String)>;
+    fn get_wm_protocols(&self, window: XWindowID) -> Option<Vec<Atom>>;
+    fn get_wm_state(&self, window: XWindowID) -> WindowState;
+    fn get_wm_transient_for(&self, window: XWindowID) -> Option<XWindowID>;
+    fn get_urgency(&self, window: XWindowID) -> bool;
 }
