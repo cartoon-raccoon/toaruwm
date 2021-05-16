3a780c32ed789fa46c1233e859c26eb1bb335f07
cartoon-raccoon <shaughn.chan@gmail.com>
2021-05-17 00:06:25 +0800

    Added screen type

---
+++ src/core/desktop.rs
@@ -0,0 +1,17 @@
+use crate::x::{XWindow, XWindowID};
+
+/// Represents a physical monitor.
+#[derive(Clone, Copy)]
+pub struct Screen {
+    pub xwindow: XWindow,
+    pub idx: i32,
+}
+
+impl Screen {
+    pub fn new(screen_idx: i32, root_id: XWindowID) -> Self {
+        Self {
+            xwindow: XWindow::from(root_id),
+            idx: screen_idx,
+        }
+    }
+}
