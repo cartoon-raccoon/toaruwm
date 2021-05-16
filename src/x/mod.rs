ea6a3fa38ac224fef7f9bd7b3e38e7e8c6e092a4
cartoon-raccoon <shaughn.chan@gmail.com>
2021-05-17 00:10:04 +0800

    Added re-exports

--- src/x/mod.rs
+++ src/x/mod.rs
@@ -2,4 +2,6 @@ pub mod core;
 pub mod ewmh;
 pub mod icccm;
 
-pub use self::core::{XConn, XWindow, XWindowID};
+pub use self::core::{XConn, XWindow, XWindowID, xproto};
+pub use icccm::Icccm;
+pub use ewmh::Ewmh;
