b6c9e3e55603d324c9bf8f1f08f6c08225f11fd8
cartoon-raccoon <shaughn.chan@gmail.com>
2021-05-16 13:58:31 +0800

    Added basic core

--- src/x/core.rs
+++ src/x/core.rs
@@ -1,3 +1,14 @@
+use crate::Geometry;
+
+pub type XWindowID = u32;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct XWindow {
+    pub id: XWindowID,
+    pub geom: Geometry,
+}
+
 pub trait XConn {
-    
+    fn get_root(&self) -> XWindowID;
+    fn get_geometry(&self, window: XWindowID) -> Geometry;
 }
