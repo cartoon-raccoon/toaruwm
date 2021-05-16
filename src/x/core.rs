a2299790ee4a5de1a51253d783b2aeb0ed5b720e
cartoon-raccoon <shaughn.chan@gmail.com>
2021-05-16 15:56:22 +0800

    Added additional impl and types

--- src/x/core.rs
+++ src/x/core.rs
@@ -1,16 +1,93 @@
-use crate::Geometry;
-use crate::XWinProperties;
+use thiserror::Error;
+
+use crate::{Geometry, XWinProperties};
 
 pub type XWindowID = u32;
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy)]
 pub struct XWindow {
     pub id: XWindowID,
     pub geom: Geometry,
 }
 
+impl PartialEq for XWindow {
+    #[inline(always)]
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+impl From<XWindowID> for XWindow {
+    fn from(from: XWindowID) -> Self {
+        Self {
+            id: from,
+            geom: Geometry {
+                x: 0,
+                y: 0,
+                height: 0,
+                width: 0,
+            }
+        }
+    }
+}
+
+impl XWindow {
+    /// Sets the geometry using an XConn object.
+    pub fn set_geometry_conn<X: XConn>(&mut self, conn: &X) {
+        match conn.get_geometry(self.id) {
+            Ok(geom) => {
+                debug!(
+                    "Updating geometry (conn):\nx: {}, y: {}, h: {}, w: {}", 
+                    geom.x, geom.y, geom.height, geom.width
+                );
+                self.geom = geom;
+            }
+
+            Err(e) => {
+                error!("{}", e);
+                return
+            }
+        }
+    }
+
+    /// Sets the geometry using a provided Geometry.
+    pub fn set_geometry(&mut self, geom: Geometry) {
+        debug!(
+            "Updating geometry for window {}:\nx: {}, y: {}, h: {}, w: {}", 
+            self.id, geom.x, geom.y, geom.height, geom.width
+        );
+        self.geom = geom;
+    }
+    /// Updates the width by a given difference.
+    pub fn update_width(&mut self, dx: u32) {
+        self.geom.width += dx;
+    }
+    /// Updates the height by given difference.
+    pub fn update_height(&mut self, dy: u32) {
+        self.geom.height += dy;
+    }
+    /// Updates the x coordinate of the window by a given difference.
+    pub fn update_pos_x(&mut self, dx: i32) {
+        self.geom.x += dx;
+    }
+    /// Updates the y coordinate of the window by a given difference.
+    pub fn update_pos_y(&mut self, dy: i32) {
+        self.geom.y += dy;
+    }
+}
+
+#[derive(Debug, Error, Clone, Copy)]
+pub enum XError {
+    #[error("Could not establish a connection to the X server.")]
+    Connection,
+    #[error("Could not complete specified request.")]
+    RequestError,
+}
+
+pub type Result<T> = ::core::result::Result<T, XError>;
+
 pub trait XConn {
     fn get_root(&self) -> XWindowID;
-    fn get_geometry(&self, window: XWindowID) -> Geometry;
+    fn get_geometry(&self, window: XWindowID) -> Result<Geometry>;
     fn get_client_properties(&self, window: XWindowID) -> XWinProperties;
 }
