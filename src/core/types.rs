9f79f3c40c8ec7791fa33ca11f2c446cd4c9e2c3
cartoon-raccoon <shaughn.chan@gmail.com>
2021-05-16 15:19:19 +0800

    Added additional types

--- src/core/types.rs
+++ src/core/types.rs
@@ -1,3 +1,5 @@
+use crate::layouts::LayoutType;
+
 pub mod keysym {
     pub use x11::keysym::*;
 }
@@ -5,6 +7,8 @@ pub mod keysym {
 pub use xcb::ModMask as ModMask;
 
 pub use crate::core::{Ring, Selector};
+
+pub type Atom = u32;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Direction {
@@ -46,4 +50,62 @@ impl Default for Geometry {
             height: 100,
         }
     }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct SizeHints {
+    pub position: Option<(i32, i32)>,
+    pub size: Option<(i32, i32)>,
+    pub min_size: Option<(i32, i32)>,
+    pub max_size: Option<(i32, i32)>,
+    pub resize: Option<(i32, i32)>,
+    pub min_aspect: Option<(i32, i32)>,
+    pub max_aspect: Option<(i32, i32)>,
+    pub base: Option<(i32, i32)>,
+    pub gravity: Option<u32>
+}
+
+#[derive(Clone, Copy, Debug)]
+pub(crate) enum WinLayoutState {
+    Tiled,
+    Floating,
+}
+
+// The ICCCM-defined window states.
+#[derive(Clone, Copy, Debug)]
+pub enum WindowState {
+    Normal,
+    Iconic,
+}
+
+impl From<LayoutType> for WinLayoutState {
+
+    #[inline]
+    fn from(from: LayoutType) -> WinLayoutState {
+        if let LayoutType::Floating = from {
+            return Self::Floating
+        }
+
+        Self::Tiled
+    }
+}
+
+/// ICCCM-defined window hints.
+#[derive(Debug, Clone, Copy)]
+pub struct WmHints {
+    pub state: WindowState,
+    pub urgent: bool,
+    //todo: add pixmaps
+}
+
+/// ICCCM-defined window properties.
+//todo: make all fields private, accessible with methods.
+pub struct XWinProperties {
+    pub(crate) wm_name: String,
+    pub(crate) wm_icon_name: String,
+    pub(crate) wm_size_hints: Option<SizeHints>,
+    pub(crate) wm_hints: Option<WmHints>,
+    pub(crate) wm_class: (String, String), //Instance, Class
+    pub(crate) wm_protocols: Option<Vec<Atom>>,
+    pub(crate) wm_state: WindowState,
 }
