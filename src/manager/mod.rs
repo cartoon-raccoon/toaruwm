987e9986570d2db01262441f1ee843dd1a2de16e
cartoon-raccoon <shaughn.chan@gmail.com>
2021-05-17 00:08:33 +0800

    Updated manager object

--- src/manager/mod.rs
+++ src/manager/mod.rs
@@ -1,17 +1,27 @@
-use crate::x::{XConn, XWindowID};
-use crate::MouseMode;
+use crate::x::{
+    XConn, 
+    XWindowID,
+    Ewmh, Icccm,
+};
+use crate::types::MouseMode;
+use crate::core::desktop::Screen;
 
-/// The main manager struct that receives and responds to events.
+/// The main window manager object that receives and responds to events.
 #[allow(dead_code)]
-pub struct WindowManager<X: XConn> {
+pub struct WindowManager<X>
+where X: XConn + Ewmh + Icccm {
     pub(crate) conn: X,
     //todo: add these
     //pub(crate) desktop: Desktop,
-    //pub(crate) screen: Screen,
+    pub(crate) screen: Screen,
     root: i32,
     mousemode: MouseMode,
     selected: Option<XWindowID>,
     last_mouse_x: i32,
     last_mouse_y: i32,
     to_quit: bool,
+}
+
+impl<X: XConn + Ewmh + Icccm> WindowManager<X> {
+
 }
