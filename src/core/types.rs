f5db4a337d9220105301404aab2c5e7c226aec94
cartoon-raccoon <shaughn.chan@gmail.com>
2021-05-16 13:59:02 +0800

    Added new types

--- src/core/types.rs
+++ src/core/types.rs
@@ -2,6 +2,9 @@ pub mod keysym {
     pub use x11::keysym::*;
 }
 
+pub use xcb::ModMask as ModMask;
+
+pub use crate::core::{Ring, Selector};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Direction {
@@ -11,14 +14,36 @@ pub enum Direction {
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Point {
-    pub x: u32,
-    pub y: u32,
+    pub x: i32,
+    pub y: i32,
 }
 
-#[derive(Debug, Clone, Copy. PartialEq)]
-pub struct Geom {
-    pub x: u32,
-    pub y: u32,
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Geometry {
+    pub x: i32,
+    pub y: i32,
     pub height: u32,
     pub width: u32,
+}
+
+// impl From<(i32, i32, i32, i32)> for Geometry {
+//     fn from(from: (i32, i32, i32, i32)) -> Self {
+//         Self {
+//             x: from.0,
+//             y: from.1,
+//             width: from.2,
+//             height: from.3,
+//         }
+//     }
+// }
+
+impl Default for Geometry {
+    fn default() -> Self {
+        Geometry {
+            x: 0,
+            y: 0,
+            width: 160,
+            height: 100,
+        }
+    }
 }
