94ce9c41d1043ff420d9f08b004cef54fd485ed0
cartoon-raccoon <shaughn.chan@gmail.com>
2021-05-17 00:10:45 +0800

    Added utility methods

---
+++ src/util.rs
@@ -0,0 +1,52 @@
+use crate::x::xproto;
+
+pub const ROOT_ATTRS: [(u32, u32); 1] = [
+    (
+        xproto::CW_EVENT_MASK, 
+        xproto::EVENT_MASK_SUBSTRUCTURE_REDIRECT | 
+        xproto::EVENT_MASK_STRUCTURE_NOTIFY |
+        xproto::EVENT_MASK_PROPERTY_CHANGE
+    )
+];
+
+// Root window mouse button event mask
+pub const ROOT_BUTTON_GRAB_MASK: xcb::ButtonMask = xproto::EVENT_MASK_BUTTON_PRESS|xproto::EVENT_MASK_BUTTON_RELEASE;
+
+// Root window pointer event mask
+pub const ROOT_POINTER_GRAB_MASK: xcb::EventMask = xproto::EVENT_MASK_BUTTON_RELEASE|xproto::EVENT_MASK_BUTTON_MOTION;
+
+pub fn cursor_attrs(cursor_id: u32) -> [(u32, u32); 1] {
+    //debug!("Getting cursor attrs for cursor {}", cursor_id);
+    return [(xproto::CW_CURSOR, cursor_id)]
+}
+
+pub fn disable_events() -> [(u32, u32); 1] {
+    //debug!("VALUES: attributes no events");
+    return [(xproto::CW_EVENT_MASK, xproto::EVENT_MASK_NO_EVENT)];
+}
+
+pub fn child_events() -> [(u32, u32); 1] {
+    //debug!("VALUES: attributes child events");
+    return [(xproto::CW_EVENT_MASK,
+        xproto::EVENT_MASK_ENTER_WINDOW|    // -> Self EnterNotify events
+        xproto::EVENT_MASK_STRUCTURE_NOTIFY // -> Self CirculateNotify, ConfigureNotify, DestroyNotify, GravityNotify, MapNotify, ReparentNotify, UnmapNotify events
+    )];
+}
+
+pub fn configure_move(x: u32, y: u32) -> [(u16, u32); 2] {
+    //debug!("VALUES: configure move");
+    return [(xproto::CONFIG_WINDOW_X as u16, x), (xproto::CONFIG_WINDOW_Y as u16, y)];
+}
+
+pub fn configure_resize(width: u32, height: u32) -> [(u16, u32); 2] {
+    //debug!("VALUES: configure resize");
+    return [(xproto::CONFIG_WINDOW_WIDTH as u16, width), (xproto::CONFIG_WINDOW_HEIGHT as u16, height)];
+}
+
+pub fn stack_above() -> [(u16, u32); 1] {
+    //debug!("VALUES: configure stack above sibling {}", window_id);
+    return [
+        (xproto::CONFIG_WINDOW_STACK_MODE as u16, xproto::STACK_MODE_ABOVE),
+        //(xproto::CONFIG_WINDOW_SIBLING as u16, window_id),
+    ];
+}
