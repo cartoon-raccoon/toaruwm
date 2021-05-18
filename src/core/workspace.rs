784d6edbc7f59083a5ed3d98e84d051b83796fdd
cartoon-raccoon <shaughn.chan@gmail.com>
2021-05-18 23:15:59 +0800

    Added workspace base

--- src/core/workspace.rs
+++ src/core/workspace.rs
@@ -2,15 +2,26 @@ use crate::core::{
     window::{Client, ClientRing},
     desktop::Screen,
 };
-use crate::layouts::LayoutType;
+use crate::layouts::{LayoutType, LayoutEngine};
 use crate::x::{XConn, XWindowID};
 
 pub struct Workspace {
     pub(crate) windows: ClientRing,
     pub(crate) master: Option<XWindowID>,
-    pub(crate) layout: LayoutType,
+    pub(crate) layoutter: LayoutEngine,
 
     _activate: fn(&dyn XConn, &mut Workspace, &Screen),
     _deactivate: fn(&dyn XConn, &mut Workspace, &Screen),
-    _add_window: fn(&dyn XConn, &mut Workspace, &Screen),
+    _add_window: fn(&dyn XConn, &mut Workspace, XWindowID, &Screen),
+    _del_window: fn(&dyn XConn, &mut Workspace, XWindowID, &Screen) -> Client,
+}
+
+impl Workspace {
+    pub fn with_layout(layout: LayoutType) -> Self {
+        todo!()
+    }
+
+    pub fn focus_window(&mut self, window: XWindowID) {
+        todo!()
+    }
 }
