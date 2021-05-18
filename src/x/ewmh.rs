469fbe7161d8827163e921419bd52289f67337ff
cartoon-raccoon <shaughn.chan@gmail.com>
2021-05-18 21:34:26 +0800

    Removed ewmh and icccm traits

--- src/x/ewmh.rs
+++ src/x/ewmh.rs
@@ -1,9 +1,3 @@
 use super::{XWindowID, XConn};
 use crate::core::types::{Atom, NetWindowStates};
 
-pub trait Ewmh: XConn {
-    fn get_window_type(&self, window: XWindowID) -> Option<Vec<Atom>>;
-    fn get_window_states(&self, window: XWindowID) -> NetWindowStates;
-    fn set_supported(&self, screen_idx: i32, atoms: &[Atom]);
-    fn set_wm_state(&self, window: XWindowID, atoms: &[Atom]);
-}
