b1e50abf43ea38564069ef6ef171c865c59912c6
cartoon-raccoon <shaughn.chan@gmail.com>
2021-06-05 16:18:14 +0800

    Added additional functionality to WM

--- src/manager/mod.rs
+++ src/manager/mod.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::process::Command;
 
 use crate::x::{
     XConn, 
@@ -19,8 +20,19 @@ pub mod state;
 pub(crate) use state::WMState;
 pub use event::EventAction;
 
+/// Some arbitrary code that can run on a certain event.
+/// 
+/// Accepts a `&mut WindowManager<X>` as a parameter, so it can
+/// manipulate internal manager state.
+pub type Hook<X> = Box<dyn FnMut(&mut WindowManager<X>)>;
+
 /// The main window manager object that owns the event loop,
 /// and receives and responds to events.
+/// 
+/// The manager is generic over a type argument X that 
+/// implements the `XConn` trait, but is never directly exposed
+/// by the type's public API and is only used when constructing
+/// a new window manager instance.
 #[allow(dead_code)]
 pub struct WindowManager<X: XConn> {
     pub(crate) conn: X,
@@ -65,7 +77,7 @@ impl<X: XConn> WindowManager<X> {
     /// Selects for subtructure redirect and notify,
     /// grabs required keys for keybinds,
     /// and runs any registered startup hooks.
-    pub fn register(&mut self) {
+    pub fn register(&mut self, hooks: Vec<Hook<X>>) {
         let root_id = self.conn.get_root();
 
         debug!("Got root id of {}", root_id);
@@ -77,6 +89,11 @@ impl<X: XConn> WindowManager<X> {
         });
 
         //conn.set_supported(sc);
+
+        // run hooks
+        for mut hook in hooks {
+            hook(self);
+        }
 
         todo!()
     }
@@ -92,10 +109,16 @@ impl<X: XConn> WindowManager<X> {
             }
         }
     }
+
+    /// Run an external command.
+    pub fn run_external(&mut self, args: &'static [&str]) {
+        
+        todo!()
+    }
     
     /// Goes to the specified workspace.
     pub fn goto_workspace(&mut self, name: &str) {
-        self.desktop.goto(&self.conn, &self.screen, name);
+        self.desktop.goto(name, &self.conn, &self.screen);
     }
     
     /// Cycles the focused workspace.
@@ -105,37 +128,60 @@ impl<X: XConn> WindowManager<X> {
 
     /// Sends a window to the specified workspace.
     pub fn send_window_to(&mut self, name: &str) {
-        self.desktop.send_window_to(&self.conn, &self.screen, name);
+        self.desktop.send_window_to(name, &self.conn, &self.screen);
     }
     
     /// Sends a window to the specified workspace and then switches to it.
     pub fn send_window_and_switch(&mut self, name: &str) {
-        self.desktop.send_window_to(&self.conn, &self.screen, name);
-        self.desktop.goto(&self.conn, &self.screen, name);
+        self.desktop.send_window_to(name, &self.conn, &self.screen);
+        self.desktop.goto(name, &self.conn, &self.screen);
     }
 
+    /// Cycles the focused window.
     pub fn cycle_focus(&mut self, direction: Direction) {
         self.desktop.current_mut().cycle_focus(&self.conn, direction);
     }
 
+    /// Toggles the state of the focused window to floating or vice versa.
     pub fn toggle_focused_state(&mut self) {
         self.desktop.current_mut().toggle_focused_state(&self.conn, &self.screen);
     }
 
+    /// Grabs the pointer and moves the window the pointer is on.
+    pub fn move_window_ptr(&mut self) {
+        todo!()
+    }
+
+    /// Grabs the pointer and resizes the window the pointer is on.
+    /// 
+    /// If the window is tiled, its state is toggled to floating.
+    pub fn resize_window_ptr(&mut self) {
+        todo!()
+    }
+
+    /// Closes the focused window
+    pub fn close_focused_window(&mut self) {
+        if let Some(window) = self.desktop.current_mut().windows.focused() {
+            self.conn.destroy_window(&window);
+        }
+    }
+
+    /// Quits the event loop.
     pub fn quit(&mut self) {
         self.to_quit = true;
     }
 
     //* Private methods
     pub(crate) fn process_next_event(&mut self) -> Vec<EventAction> {
-        let action = self.conn.get_next_event();
-
-        EventAction::from_xevent(action, self.state())
+        EventAction::from_xevent(
+            self.conn.get_next_event(), 
+            self.state()
+        )
     }
 
     fn handle_event(&mut self, actions: Vec<EventAction>) -> Result<()> {
         for _action in actions {
-            todo!()
+            todo!()  //* match events and run functions accordingly
         }
 
         Ok(())
