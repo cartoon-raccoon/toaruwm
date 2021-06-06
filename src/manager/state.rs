bb2b72c030960ac23ce2c8b27057eb30e7d8334c
cartoon-raccoon <shaughn.chan@gmail.com>
2021-06-06 23:32:06 +0800

    Added documentation for state

--- src/manager/state.rs
+++ src/manager/state.rs
@@ -3,7 +3,9 @@ use super::{WindowManager, MouseMode};
 use crate::core::{Ring, Workspace};
 use crate::x::{XConn, XWindowID};
 
-
+/// Provides a view into the state of the window manager.
+///
+/// Used as a context when generating event actions.
 pub(crate) struct WMState<'a, X: XConn> {
     pub conn: &'a X,
     pub workspaces: &'a Ring<Workspace>,
@@ -13,7 +15,6 @@ pub(crate) struct WMState<'a, X: XConn> {
 }
 
 impl<X: XConn> WindowManager<X> {
-    #[allow(dead_code)]
     pub(crate) fn state(&self) -> WMState<X> {
         WMState {
             conn: &self.conn,
