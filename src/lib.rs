d02378804ec218e21f7ba63fdab82cad88725bbe
cartoon-raccoon <shaughn.chan@gmail.com>
2021-06-06 12:51:19 +0800

    Added convenience constructor function

--- src/lib.rs
+++ src/lib.rs
@@ -10,5 +10,19 @@ pub mod keybinds;
 pub(crate) mod util;
 
 pub use crate::core::types;
+pub use crate::core::types::Result;
+pub use crate::x::core::Result as XResult;
 pub use crate::manager::WindowManager;
+
+use crate::x::xcb::XCBConn;
+
+/// Convenience function for creating a XCB-backed WindowManager.
+pub fn xcb_backed_wm() -> XResult<WindowManager<XCBConn>> {
+    let mut xcbconn = XCBConn::connect()?;
+    xcbconn.init()?;
+
+    let wm = WindowManager::new(xcbconn);
+
+    Ok(wm)
+}
 
