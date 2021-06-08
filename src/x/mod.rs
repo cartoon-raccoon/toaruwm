82756101b802a5b8cc124e861e11c2a20ddbeb08
cartoon-raccoon <shaughn.chan@gmail.com>
2021-06-08 23:31:16 +0800

    Updated re-exports

--- src/x/mod.rs
+++ src/x/mod.rs
@@ -18,7 +18,9 @@ pub mod xcb;
 pub mod atom;
 pub mod property;
 
-pub use self::core::{XConn, XWindow, XWindowID, XAtom, xproto};
+pub use self::core::{
+    XConn, XWindow, XWindowID, XAtom, XError, xproto
+};
 pub use event::XEvent;
 pub use atom::{Atom, Atoms};
 pub use property::*;
