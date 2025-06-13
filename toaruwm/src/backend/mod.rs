8062b678087157574b3ee6e9818aa7fc24d351bf
Sam <shaughn.chan@gmail.com>
2025-06-13 19:25:41 -0400

    wayland: started work on wayland

--- toaruwm/src/backend/mod.rs
+++ toaruwm/src/backend/mod.rs
@@ -1,1 +1,17 @@
+//! Traits and structs for the backend of the [WindowManager].
+
+/// Backends for the X11 server.
 pub mod x;
+/// Backends for Wayland.
+pub mod wayland;
+
+/// An object that can serve as the backend of a [WindowManager].
+pub trait Backend {
+    /// The name of the backend.
+    fn name(&self) -> &str;
+
+    /// Runs the event loop, responding to events as they come in.
+    fn run_event_loop(&mut self);
+
+    
+}
